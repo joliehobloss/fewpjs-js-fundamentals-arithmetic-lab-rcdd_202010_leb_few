f0b13c4da54a1dccbf7ab82c30ac5971ac257c84
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-24 13:17:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 let num1=1;
 let num2=62;
 const multiply=num1*num2;
-const max=Math.max(4, 2, 10, 20, 5);
 const random=Math.random() * (100 - 1);
+const mod=
+const max=Math.max(4, 2, 10, 20, 5);
+
 
