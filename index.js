9b907fa37100f48ee15f66ecc348af8f994fa653
Jolie Hobloss <jolieshobloss@gmail.com>
2020-11-24 13:19:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 // Write your code here
-let num1=1;
-let num2=62;
-
 
 function multiple(num1,num2){
   const multiply=num1*num2;
+  return multiply;
 }
-const random=Math.random() * (100 - 1);
+/*const random=Math.random() * (100 - 1);
 const mod=
-const max=Math.max(4, 2, 10, 20, 5);
+const max=Math.max(4, 2, 10, 20, 5);*/
 
 
